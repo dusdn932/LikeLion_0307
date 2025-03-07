57e8c10dac5f1cbc150845fc52f3c1b2f72d0b2b
dusdn932 <50188283+dusdn932@users.noreply.github.com>
2025-03-07 14:03:11 +0900

    0307_오후강의1
    공튀기기 게임(미완)
    이따가 더 구현

---
+++ ConsoleApp2/ConsoleApp2/BALLDATA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class BALLDATA
+    {
+        public int nReady; //공 움직일 준비상태
+        public int nDirect; //공방향변수
+        public int nX, nY; //x좌표 y좌표
+
+    }
+}
